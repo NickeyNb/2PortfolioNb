c97d8195bbeede5fcf7ec4a8f5bd34eee15b5565
Nitin <nitinnb31122002@gmail.com>
2023-01-21 10:41:52 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+//     const scriptURL = 'https://script.google.com/macros/s/AKfycbweHv6z_H4_UMO8SEInGJqY3Eb6dWSOxRc16SQAWZYFMZfRu2guaCGcOuBFYze6TS82/exec'
+//     const form = document.forms['submit-to-google-sheet']
+
+//   form.addEventListener('submit', e => {
+//         e.preventDefault()
+//     fetch(scriptURL, {method: 'POST', body: new FormData(form)})
+//       .then(response => console.log('Success!', response))
+//       .catch(error => console.error('Error!', error.message))
+//   })
