0d70ee90cfbf6ebeba8cc2ec79ea40091d409bb1
Nitin Bisht <110400673+NickeyNb@users.noreply.github.com>
2023-02-02 16:27:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+const nav_header = document.querySelector(".nv");
 
-//     const scriptURL = 'https://script.google.com/macros/s/AKfycbweHv6z_H4_UMO8SEInGJqY3Eb6dWSOxRc16SQAWZYFMZfRu2guaCGcOuBFYze6TS82/exec'
-//     const form = document.forms['submit-to-google-sheet']
+const toggleNavbar = () => {
+  nav_header.classList.toggle("active");
+};
 
-//   form.addEventListener('submit', e => {
-//         e.preventDefault()
-//     fetch(scriptURL, {method: 'POST', body: new FormData(form)})
-//       .then(response => console.log('Success!', response))
-//       .catch(error => console.error('Error!', error.message))
-//   })
+mobile_nav.addEventListener("click", () => toggleNavbar());
+
