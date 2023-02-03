e20d55370dc262c0b5ca7b19ceee11cae2c908b1
Nitin Bisht <110400673+NickeyNb@users.noreply.github.com>
2023-02-03 18:25:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,27 @@ const toggleNavbar = () => {
 
 mobile_nav.addEventListener("click", () => toggleNavbar());
 
+
+const sr = ScrollReveal({
+  distance:'50px',
+  duration:2000,
+  reset:true
+})
+
+sr.reveal('.left, .about-col-1, .Project, .contactInfo',{delay:170, origin:'left'});
+sr.reveal('.right,.about-col-2, .contactForm, .endIcons, .mySkills1',{delay:170, origin:'right'});
+sr.reveal('.headingAbout',{delay:170, origin:'top'});
+sr.reveal('.last, .publish, .arrow_btn',{delay:170, origin:'bottom'});
+
+
+
+// window.addEventListener('scroll', 
+// function() {
+//   var navBarr = document.getElementById('navBarr');
+//   if(window.pageYOffset >= 738.9) {
+//     navBarr.classList.add('sticky');
+
+//   } else {
+//     navBarr.classList.remove("sticky");
+//   }
+// })
